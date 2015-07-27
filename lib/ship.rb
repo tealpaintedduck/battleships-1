7ab6fdc31f89607bf301d9223af713712ad60a8c
Andrew Stelmach <andrew_stelmach@hotmail.com>
2015-07-27 17:26:26 +0100

    small refactor

--- lib/ship.rb
+++ lib/ship.rb
@@ -2,13 +2,17 @@ class Ship
   attr_reader :location, :type
 
   def initialize(type, location)
-    fail 'Incorrect ship type' unless ship_types.has_key?(type)
+    fail 'Incorrect ship type' unless valid_ship_type?(type)
     @location = location
     @type = type
   end
 
   def ship_types
     {patrol_boat: 2, destroyer: 3, submarine: 3, battleship: 4, aircraft_carrier: 5}
+  end
+
+  def valid_ship_type?(type)
+  	ship_types.has_key?(type)
   end
 
 end
