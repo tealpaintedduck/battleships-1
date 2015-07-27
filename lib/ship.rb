60498398ae130712a35d381754aed026b49ad3cd
Andrew Stelmach <andrew_stelmach@hotmail.com>
2015-07-27 12:52:05 +0100

    ships created with a location and a board

--- lib/ship.rb
+++ lib/ship.rb
@@ -1,7 +1,9 @@
 class Ship
   attr_reader :location
+  attr_reader :board
 
-  def initialize(location)
+  def initialize(location, board)
     @location = location
+    @board = board
   end
 end
--- spec/ship_spec.rb
+++ spec/ship_spec.rb
@@ -1,13 +1,18 @@
 require 'ship'
 
 describe Ship do
+
+	let (:board){ double :board }
+
   before(:each) do
-    @ship = Ship.new('')
+    @ship = Ship.new('', board)
   end
 
   it 'can be created with a location' do
     expect(@ship.location).to be_a String
   end
 
-  it ''
+  it 'can be created with a board' do 
+  	expect(@ship.board).to be_truthy
+  end
 end
